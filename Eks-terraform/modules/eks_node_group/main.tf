a576580198554954988d35661b431e1d17e91cec
kishore23698 <kishorekumar.p@indiumsoft.com>
2024-02-07 18:50:48 +0530

    nodegrp scaling size changed

--- Eks-terraform/modules/eks_node_group/main.tf
+++ Eks-terraform/modules/eks_node_group/main.tf
@@ -45,9 +45,9 @@ resource "aws_eks_node_group" "node_group" {
   tags = var.tags
   
   scaling_config {
-    desired_size = 1
+    desired_size = 2
     min_size     = 1
-    max_size     = 1
+    max_size     = 3
   }
   depends_on = [aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy, aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly, aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy]
 
